86cf77deaf767c5a1c503d72d011d5c390c967c1
prac01 <54296838+prac01@users.noreply.github.com>
2019-08-19 23:00:30 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function(){
+	var navOffset = jQuery("nav").offset().top;
+	
+	jQuery("nav").wrap('<div class="nav-placeholder"></div>');
+	jQuery(".nav-placeholder").height(jQuery("nav").outerHeight());
+	
+	jQuery("nav").wrapInner('<div class="nav-inner"></div>');
+	
+	jQuery(window).scroll(function() {
+		var scrollPos = jQuery(window).scrollTop();
+		
+		if (scrollPos >= navOffset){
+			jQuery("nav").addClass("fixed");
+		}else{
+			jQuery("nav").removeClass("fixed");
+		}
+		
+	});
+	
+	
+	
+});
+
